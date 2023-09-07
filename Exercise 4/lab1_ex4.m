6aa66a6c241243adccf7ad3224f5bdcb6bbb007b
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-09-07 13:04:21 +0200

    Update Exercise 4 with Inverse Euler formulas

--- Exercise 4/lab1_ex4.m
+++ Exercise 4/lab1_ex4.m
@@ -8,40 +8,64 @@
 % imaginary part separately.)
 
 %% Generate signals
-n1 = 0:20;
+n1 = 0:20; % Times
 n2 = 0:50;
-x1 = 3 * sin(pi/7 * n1) + 4j * cos(pi/7 * n1);
-x2 = (1.1).^n2 .* cos(pi/11 * n2 + pi/4);
-x11 = real(exp(1j * pi/7 * n1)) * (4j + 3);
-x22 = real((1.1).^n2 .* exp(1j * (pi/11 * n2 + pi/4)));
+f1 = pi/7; % Freq
+f2 = pi/11;
+p2 = pi/4; % Phase
+
+t1 = f1*n1; % Aliases
+t2 = f2*n2+p2;
+
+x1 = 3 * sin(f1 * n1) + 4j * cos(f1 * n1);
+x2 = (1.1).^n2 .* cos(f2 * n2 + pi/4);
+
+x11 = 3 * (exp(1j*t1) - exp(-1j*t1))/2j + 4j * (exp(1j*t1) + exp(-1j*t1))/2;
+x22 = (1.1).^n2 .* (exp(1j*t2) + exp(-1j*t2))/2;
+
+x111 = 3*imag(exp(1j * pi/7 * n1)) + 4j*real(exp(1j * pi/7 * n1));
+x222 = (1.1).^n2 .* real(exp(1j * (f2 * n2 + pi/4)));
 
 %% Plot signals
 figure(1);
 
 % Figure 1.1: x1[·]
-subplot(3, 2, 1);
+subplot(3, 3, 1);
 plot(n1, real(x1));
 title('x_1[·] (real)'), xlabel('n'), ylabel('Re(x_1[n])');
-subplot(3, 2, 3);
+subplot(3, 3, 4);
 plot(n1, imag(x1));
 title('x_1[·] (imaginary)'), xlabel('n'), ylabel('Im(x_1[n])');
 
 % Figure 1.2: x11[·]
-subplot(3, 2, 2);
+subplot(3, 3, 2);
 plot(n1, real(x11));
 title('x_{11}[·] (real)'), xlabel('n'), ylabel('Re(x_{11}[n])');
-subplot(3, 2, 4);
+subplot(3, 3, 5);
 plot(n1, imag(x11));
 title('x_{11}[·] (imaginary)'), xlabel('n'), ylabel('Im(x_{11}[n])');
 
+% Figure 1.2: x111[·]
+subplot(3, 3, 3);
+plot(n1, real(x111));
+title('x_{111}[·] (real)'), xlabel('n'), ylabel('Re(x_{111}[n])');
+subplot(3, 3, 6);
+plot(n1, imag(x111));
+title('x_{111}[·] (imaginary)'), xlabel('n'), ylabel('Im(x_{111}[n])');
+
 % Figure 1.3: x2[·]
-subplot(3, 2, 5);
+subplot(3, 3, 7);
 plot(n2, x2);
 title('x_2[·]'), xlabel('n'), ylabel('x_2[n]');
 
 % Figure 1.4: x22[·]
-subplot(3, 2, 6);
+subplot(3, 3, 8);
 plot(n2, x22);
 title('x_{22}[·]'), xlabel('n'), ylabel('x_{22}[n]');
+
+% Figure 1.4: x222[·]
+subplot(3, 3, 9);
+plot(n2, x222);
+title('x_{222}[·]'), xlabel('n'), ylabel('x_{222}[n]');
 
 
